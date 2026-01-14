d7f57cfc0fc8b43bed8eda552a55f4f505d52571
Akarsh Sujay Kumar <akarshsujaykumar.23is@saividya.ac.in>
2026-01-14 12:00:00 +0530

    feat: define supported file extensions list

--- rag_engine.py
+++ rag_engine.py
@@ -8,3 +8,6 @@ from config import LLM_PROVIDER, DATA_DIR
 
 
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+
+
+SUPPORTED_EXTENSIONS = ['.pdf', '.txt', '.md', '.docx']
